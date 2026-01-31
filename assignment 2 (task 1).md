2dea60366f8c7cc4c58acdf34eb6fc736244f107
ankitmaurya842347-dev <ankitmaurya842347@gmail.com>
2026-01-31 21:33:46 +0530

    Add files via upload

---
+++ assignment 2 (task 1).md
@@ -0,0 +1,3 @@
+num = int(input("Enter a Number: "))
+
+print(f"{num } is Even") if num % 2 == 0 else print(f"{num } is Odd")
---
+++ assinment 2 (task 2).py
@@ -0,0 +1,7 @@
+num = int(input("Enter strat number"))
+num2 = int(input("Enter last number"))
+add = 0
+for i in range(num,num2+1):
+    add += i
+    
+print(add)
